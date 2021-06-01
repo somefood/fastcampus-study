b368c5014c581187b488a493e142464b7f63ca37
Seokju Hong <iamseokjuhong@gmail.com>
2021-06-01 18:42:43 +0900

    git init

---
+++ java-basic/src/chap02/variable/BooleanTest.java
@@ -0,0 +1,10 @@
+package chap02.variable;
+
+public class BooleanTest {
+    public static void main(String[] args) {
+        boolean isMarried = false;
+
+        System.out.println(isMarried);
+        
+    }
+}
---
+++ java-basic/src/chap02/variable/DoubleTest.java
@@ -0,0 +1,10 @@
+package chap02.variable;
+
+public class DoubleTest {
+    public static void main(String[] args) {
+        double dNum = 3.14;
+
+        float fNum = 3.14f;
+        System.out.println(dNum + " " + fNum);
+    }
+}
