d36bc6372efcc44379f4bbb33eb456ca68c61eb3
Seokju Hong <iamseokjuhong@gmail.com>
2021-06-25 23:23:38 +0900

    챕터10 시작

--- java-basic/src/chap10/object/ToStringTest.java
+++ java-basic/src/chap10/object/ToStringTest.java
@@ -1,6 +1,6 @@
 package chap10.object;
 
-class Book{
+class Book implements Cloneable{
     String title;
     String author;
 
@@ -13,15 +13,25 @@ class Book{
     public String toString() {
         return author + ", " + title;
     }
+
+    // 클론시 CLonable 인터페이스 Implemnt 필요
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
 }
 
 public class ToStringTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
 
         Book book = new Book("토지2", "박경리");
         System.out.println(book);
 
         String str = new String("토지");
         System.out.println(str);
+
+        Book book2 = (Book) book.clone();
+        System.out.println(book2);
     }
 }
