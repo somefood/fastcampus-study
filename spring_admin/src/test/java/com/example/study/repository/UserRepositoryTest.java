342623a7484094ccffc086ed73ef82525ea40568
Seokju Hong <iamseokjuhong@gmail.com>
2021-07-09 15:38:10 +0900

    스프링 부트 프로젝트 10 JPA를 통한 CRUD 배우기 - 1_2

--- spring_admin/src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ spring_admin/src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -3,6 +3,8 @@ package com.example.study.repository;
 import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.User;
 import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -26,12 +28,27 @@ public class UserRepositoryTest extends StudyApplicationTests{
         System.out.println("newUser : " + newUser);
     }
 
+    @Test
     public void read() {
+        Optional<User> user = userRepository.findById(2L); // Long 타입
 
+        user.ifPresent(selectUser -> {
+            System.out.println("user : " + selectUser);
+            System.out.println("email : " + selectUser.getEmail());
+        });
     }
 
+    @Test
     public void update() {
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("pppp");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
     }
 
     public void delete() {
