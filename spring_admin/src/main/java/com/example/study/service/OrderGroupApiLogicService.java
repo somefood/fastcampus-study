0cfb3469daf39755c05badcba77e1a9dcba32fa1
Seokju Hong <iamseokjuhong@gmail.com>
2021-07-20 19:48:06 +0900

    스프링 부트 프로젝트 8-3-03 주문내역 API UPDATE

--- spring_admin/src/main/java/com/example/study/service/OrderGroupApiLogicService.java
+++ spring_admin/src/main/java/com/example/study/service/OrderGroupApiLogicService.java
@@ -55,7 +55,27 @@ public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiReq
 
     @Override
     public Header<OrderGroupApiResponse> update(Header<OrderGroupApiRequest> request) {
-        return null;
+        OrderGroupApiRequest body = request.getData();
+
+        return orderGroupRepository.findById(body.getId())
+                .map(orderGroup -> {
+                    orderGroup
+                            .setStatus(body.getStatus())
+                            .setOrderType(body.getOrderType())
+                            .setRevAddress(body.getRevAddress())
+                            .setRevName(body.getRevName())
+                            .setPaymentType(body.getPaymentType())
+                            .setTotalPrice(body.getTotalPrice())
+                            .setTotalQuantity(body.getTotalQuantity())
+                            .setOrderAt(body.getOrderAt())
+                            .setArrivalDate(body.getArrivalDate())
+                            .setUser(userRepository.getById(body.getUserId()))
+                            ;
+                    return orderGroup;
+                })
+                .map(changeOrderGroup -> orderGroupRepository.save(changeOrderGroup))
+                .map(newOrderGroup -> response(newOrderGroup))
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
     }
 
     @Override
