a42399ca10f434b25caaf958244df09823717738
Seokju Hong <iamseokjuhong@gmail.com>
2021-07-26 16:26:38 +0900

    지인 정보 관리 시스템 만들기 JPA Relation - 2

--- mycontact/src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Block.java
+++ mycontact/src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Block.java
@@ -1,6 +1,9 @@
 package com.fastcampus.javaallinone.project3.mycontact.domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,11 +12,14 @@ import java.time.LocalDate;
 
 @Entity
 @Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Block {
     @Id
     @GeneratedValue
     private Long id;
 
+    @NonNull
     private String name;
 
     private String reason;
--- mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
+++ mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
@@ -1,10 +1,14 @@
 package com.fastcampus.javaallinone.project3.mycontact.service;
 
+import com.fastcampus.javaallinone.project3.mycontact.domain.Block;
+import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
 import com.fastcampus.javaallinone.project3.mycontact.repository.BlockRepository;
 import com.fastcampus.javaallinone.project3.mycontact.repository.PersonRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,6 +25,38 @@ class PersonServiceTest {
 
     @Test
     void getPeopleExcludeBlocks() {
+        givenPeople();
+        givenBlocks();
 
+        List<Person> result = personService.getPeopleExcludeBlocks();
+
+//        System.out.println(result);
+        result.forEach(System.out::println);
+    }
+
+    private void givenPeople() {
+        givenPerson("seokju", 10, "A");
+        givenPerson("martin", 9, "B");
+        givenPerson("david", 7, "O");
+        givenBlockPerson("seokju", 11, "AB");
+    }
+
+    private void givenBlocks() {
+        givenBlock("seokju");
+    }
+
+    private void givenBlockPerson(String name, int age, String bloodType) {
+        Person blockPerson = new Person(name, age, bloodType);
+        blockPerson.setBlock(givenBlock(name));
+
+        personRepository.save(blockPerson);
+    }
+
+    private void givenPerson(String name, int age, String bloodType) {
+        personRepository.save(new Person(name, age, bloodType));
+    }
+
+    private Block givenBlock(String name) {
+        return blockRepository.save(new Block(name));
     }
 }
