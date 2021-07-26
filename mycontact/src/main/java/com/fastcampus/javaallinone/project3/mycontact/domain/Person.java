9a7079cb0a1e15469c27f0c2636be91ce1c81036
Seokju Hong <iamseokjuhong@gmail.com>
2021-07-26 11:56:20 +0900

    지인 정보 관리 시스템 만들기 Lombok(2) - 1

--- mycontact/src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
+++ mycontact/src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -1,49 +1,58 @@
 package com.fastcampus.javaallinone.project3.mycontact.domain;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Person {
     @Id
     @GeneratedValue // 자동 생성
     private Long id;
 
+    @NonNull
     private String name;
 
+    @NonNull
     private int age;
 
-    public Long getId() {
-        return id;
-    }
+    private String hobby;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private String bloodType;
 
-    public String getName() {
-        return name;
-    }
+    private String address;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    private LocalDate birthday;
 
-    public int getAge() {
-        return age;
-    }
+    private String job;
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+    @ToString.Exclude
+    private String phoneNumber;
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
+    public boolean equals(Object object) {
+        if(object == null){
+            return false;
+        }
+
+        Person person = (Person) object;
+
+        if(!person.getName().equals(this.getName())) {
+            return false;
+        }
+
+        if(person.getAge() != this.getAge()) {
+            return false;
+        }
+
+        return true;
     }
 }
--- mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
+++ mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,15 +22,27 @@ class PersonRepositoryTest {
         Person person = new Person();
         person.setName("seokju");
         person.setAge(10);
+        person.setBloodType("A");
 
         personRepository.save(person);
 
-//        System.out.println(personRepository.findAll());
+        System.out.println(personRepository.findAll());
 
         List<Person> people = personRepository.findAll();
 
         Assertions.assertThat(people.size()).isEqualTo(1);
         Assertions.assertThat(people.get(0).getName()).isEqualTo("seokju");
         Assertions.assertThat(people.get(0).getAge()).isEqualTo(10);
+        Assertions.assertThat(people.get(0).getBloodType()).isEqualTo("A");
+    }
+
+    @Test
+    void hashCodeAndEquals() {
+        Person person1 = new Person("seokju", 10);
+        Person person2 = new Person("seokju", 10);
+
+        System.out.println(person1.equals(person2));
+        System.out.println(person1.hashCode());
+        System.out.println(person2.hashCode());
     }
 }
