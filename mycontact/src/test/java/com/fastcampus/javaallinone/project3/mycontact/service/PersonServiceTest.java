39f9f651736d53f919d9a521175af7751598f3f9
Seokju Hong <iamseokjuhong@gmail.com>
2021-07-27 11:10:44 +0900

    지인 정보 관리 시스템 만들기 JPA Query Method

--- mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
+++ mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
@@ -34,6 +34,15 @@ class PersonServiceTest {
     }
 
     @Test
+    void getPeopleByName() {
+        givenPeople();
+
+        List<Person> result = personService.getPeopleByName("seokju");
+
+        result.forEach(System.out::println);
+    }
+
+    @Test
     void cascadeTest() {
         givenPeople();
 
