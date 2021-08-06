26d89cf2118fc8d167c953a8704f5824073a2bd0
Seokju Hong <iamseokjuhong@gmail.com>
2021-08-06 23:03:24 +0900

    지인 정보 관리 시스템 만들기 Mock-Test 01. Service Test #1

--- mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
+++ mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
@@ -3,31 +3,34 @@ package com.fastcampus.javaallinone.project3.mycontact.service;
 import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
 import com.fastcampus.javaallinone.project3.mycontact.repository.PersonRepository;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-@SpringBootTest
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
-    @Autowired
+    @InjectMocks
     private PersonService personService;
-    @Autowired
+    @Mock
     private PersonRepository personRepository;
 
     @Test
     void getPeopleByName() {
+        when(personRepository.findByName("martin"))
+                .thenReturn(Lists.newArrayList(new Person("martin")));
+
         List<Person> result = personService.getPeopleByName("martin");
 
         Assertions.assertThat(result.size()).isEqualTo(1);
         Assertions.assertThat(result.get(0).getName()).isEqualTo("martin");
-    }
-
-    @Test
-    void getPerson() {
-        Person person = personService.getPerson(3L);
-
-        Assertions.assertThat(person.getName()).isEqualTo("dennis");
     }
 }
